1b9678add0db467269884547d7282f68a9a1d78f
Fidelis-ngotho <fidelngotho2@gmail.com>
2024-11-20 17:25:55 +0300

    add all changes done

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Book, Library
+
+# Register the models to appear in the admin interface
+admin.site.register(Book)
+admin.site.register(Library)
+
 
 # Register your models here.
