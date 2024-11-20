9ee1e292fbc92e343a0080f7665e1a48745de038
Fidelis-ngotho <fidelngotho2@gmail.com>
2024-11-20 22:00:34 +0300

    add changes done to urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,20 +11,14 @@ urlpatterns = [
 
     # Register view
     path('register/', views.register, name='register'),
-]
 
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
+    # Role-based views
     path('admin/', views.admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
-]
-urlpatterns = [
+
+    # Book management views
     path('book/add/', views.add_book, name='add_book'),
     path('book/edit/<int:pk>/', views.edit_book, name='edit_book'),
     path('book/delete/<int:pk>/', views.delete_book, name='delete_book'),
 ]
-
