8d8b4e6e17b3deb6e4264d8325a1a969e02ff345
Fidelis-ngotho <fidelngotho2@gmail.com>
2024-11-20 18:36:26 +0300

    add all changes done to the views.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,28 @@
-from django.shortcuts import render
-from django.views.generic.detail import DetailView
-from .models import Library
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Function-based view to list all books
-def list_books(request):
-    books = Book.objects.all()  # Exact query expected by the checker
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+def user_register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
 
-# Class-based view for library details
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Replace 'home' with your desired redirect URL
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return render(request, 'logout.html')
