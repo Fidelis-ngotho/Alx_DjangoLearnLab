5d562bff0133086e8926e6f3df889bfe7414bae0
Fidelis-ngotho <fidelngotho2@gmail.com>
2024-11-20 21:34:08 +0300

    add changes done to views. py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -27,3 +27,33 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return render(request, 'logout.html')
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+
+# Helper function to check if the user is an Admin
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+# Helper function to check if the user is a Librarian
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+# Helper function to check if the user is a Member
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship-app/member_view.html')
+
