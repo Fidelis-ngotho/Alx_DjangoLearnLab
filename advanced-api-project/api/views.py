b298e996b67e6c2ea6e65f45015f51c1cebb1f25
Fidelis-ngotho <fidelngotho2@gmail.com>
2024-12-04 17:07:00 +0300

    add all

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,7 +4,8 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
+from rest_framework import generics
 
 # List all books
 class BookListView(ListAPIView):
