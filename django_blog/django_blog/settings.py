19c2642b72aa6c5659bbe2b3a1fb10d07ff455eb
Fidelis-ngotho <fidelngotho2@gmail.com>
2024-12-06 20:39:21 +0300

    add all

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog_post',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
         'USER': 'fidel',
         'PASSWORD': 'fidel2022',
         'HOST': 'localhost',  # Default host
