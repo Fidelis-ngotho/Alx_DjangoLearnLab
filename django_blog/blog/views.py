e322c306e1d8f954ffbd7460cd2c1a3e9c613ca7
Fidelis-ngotho <fidelngotho2@gmail.com>
2024-12-06 23:22:11 +0300

    add all

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
-from .forms import CustomUserCreationForm
 
 def register(request):
     if request.method == 'POST':
