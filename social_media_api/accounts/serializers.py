a840dbd8be816e029bde2d565e4201360e09d7af
Fidelis-ngotho <fidelngotho2@gmail.com>
2024-12-09 22:17:10 +0300

    add all

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import CustomUser
+from rest_framework.authtoken.models import Token
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
