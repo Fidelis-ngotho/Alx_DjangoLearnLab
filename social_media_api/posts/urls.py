7ce6e136f858d5575ddeb6726717e0d9f6c67bbf
Fidelis-ngotho <fidelngotho2@gmail.com>
2024-12-15 18:50:40 +0300

    add all

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -6,3 +6,10 @@ router.register(r'posts', PostViewSet, basename='post')
 router.register(r'comments', CommentViewSet, basename='comment')
 
 urlpatterns = router.urls
+
+
+urlpatterns = [
+    # Add path for the feed
+    path('feed/', views.FeedView.as_view(), name='feed'),  # Includes "feed/"
+]
+
