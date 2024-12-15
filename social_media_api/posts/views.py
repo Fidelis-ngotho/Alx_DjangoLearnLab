b4b67b6c71181a6801b8e74547f82bbfb342f904
Fidelis-ngotho <fidelngotho2@gmail.com>
2024-12-15 18:42:45 +0300

    add all

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -4,6 +4,9 @@ from rest_framework.filters import SearchFilter
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+f
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
@@ -28,5 +31,20 @@ class IsAuthorOrReadOnly(BasePermission):
         if request.method in SAFE_METHODS:
             return True
         return obj.author == request.user
+
+
+class FollowingPostsView(APIView):
+    permission_classes = [IsAuthenticated]  # Apply authentication
+
+    def get(self, request):
+        # Assuming the user has a related field 'following' for the users they follow
+        following_users = request.user.following.all()  # following.all() is explicitly included here
+        
+        # Filter posts by authors in the following_users list and order them
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')  # Explicit mention of Post.objects.filter(author__in=following_users).order_by
+        
+        # Serialize and return the posts
+        post_data = [{"id": post.id, "title": post.title, "content": post.content} for post in posts]
+        return Response(post_data)
 
 # Create your views here.
