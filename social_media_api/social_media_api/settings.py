3bbf6092b6ee93386ef1d37d7d53f4f0eaee7b1d
Fidelis-ngotho <fidelngotho2@gmail.com>
2024-12-21 03:51:36 +0300

    add all

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -142,3 +142,17 @@ SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = 'DENY'
 SECURE_SSL_REDIRECT = True  # Ensure HTTPS
+
+import os
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your-secret-key')
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
